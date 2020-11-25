7a53c6830c123ac439474f6fff2d89b48199c244
Kristof Buts <kristof.buts@marqiz.com>
2020-11-26 00:02:54 +0100

    add prefix

---
+++ src/api/DnsWatcher.Persistence/Migrations/20201125230215_AddRecordPrefix.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DnsWatcher.Persistence.Migrations
+{
+    public partial class AddRecordPrefix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Prefix",
+                table: "WatchedRecords",
+                type: "character varying(512)",
+                maxLength: 512,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Prefix",
+                table: "WatchedRecords");
+        }
+    }
+}
