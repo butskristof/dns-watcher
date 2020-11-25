68f6cddf2798d259d460224b9921bdc1ff91567b
Kristof Buts <kristof.buts@marqiz.com>
2020-11-25 20:26:03 +0100

    entities

---
+++ src/api/DnsWatcher.Domain/Entities/Domains/Domain.cs
@@ -0,0 +1,9 @@
+﻿using DnsWatcher.Domain.Common;
+
+namespace DnsWatcher.Domain.Entities.Domains
+{
+	public class Domain : EntityBase
+	{
+		public string DomainName { get; set; }
+	}
+}
---
+++ src/api/DnsWatcher.Domain/Entities/Servers/DnsServer.cs
@@ -0,0 +1,11 @@
+﻿using DnsWatcher.Domain.Common;
+
+namespace DnsWatcher.Domain.Entities.Servers
+{
+	public class DnsServer : EntityBase
+	{
+		public string Name { get; set; }
+		public string IpAddress { get; set; }
+		public int Port { get; set; } = 53;
+	}
+}
