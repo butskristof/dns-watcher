33bb720284db62277b6a1f246dc8afda00a4abd9
Kristof Buts <kristof.buts@marqiz.com>
2020-11-25 23:13:43 +0100

    entities

--- src/api/DnsWatcher.Domain/Entities/Domains/WatchedDomain.cs
+++ src/api/DnsWatcher.Domain/Entities/Domains/WatchedDomain.cs
@@ -1,9 +1,12 @@
-﻿using DnsWatcher.Domain.Common;
+﻿using System.Collections.Generic;
+using DnsWatcher.Domain.Common;
 
 namespace DnsWatcher.Domain.Entities.Domains
 {
 	public class WatchedDomain : EntityBase
 	{
 		public string DomainName { get; set; }
+
+		public ICollection<WatchedRecord> WatchedRecords { get; set; }
 	}
 }
---
+++ src/api/DnsWatcher.Domain/Entities/Domains/WatchedRecord.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using DnsWatcher.Domain.Common;
+using DnsWatcher.Domain.Enumerations;
+
+namespace DnsWatcher.Domain.Entities.Domains
+{
+	public class WatchedRecord : EntityBase
+	{
+		public RecordType RecordType { get; set; }
+		public string ExpectedIpAddress { get; set; }
+		public int ExpectedPort { get; set; }
+		
+		public Guid WatchedDomainId { get; set; }
+		public WatchedDomain WatchedDomain { get; set; }
+	}
+}
