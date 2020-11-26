c7adda5d5dfdde45c713014431fe91717dc082bd
Kristof Buts <kristof.buts@marqiz.com>
2020-11-26 18:14:32 +0100

    basic setup

--- src/pwa/src/app/app-routing.module.ts
+++ src/pwa/src/app/app-routing.module.ts
@@ -2,6 +2,19 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [];
+//   {
+//     path: 'auth',
+//     loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+//   },
+//   {
+//     path: 'browse',
+//     loadChildren: () => import('./browse/browse.module').then(m => m.BrowseModule)
+//   },
+//   {
+//     path: '**',
+//     redirectTo: '/browse'
+//   }
+// ]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
