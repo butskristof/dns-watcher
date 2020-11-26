4294c3531b219bf8ccf98bb6f74c67371beeb044
Kristof Buts <kristof.buts@marqiz.com>
2020-11-26 17:58:11 +0100

    additional record resolving

--- src/api/DnsWatcher.Infrastructure/Services/DnsResolver.cs
+++ src/api/DnsWatcher.Infrastructure/Services/DnsResolver.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using DnsClient;
@@ -13,21 +14,42 @@ namespace DnsWatcher.Infrastructure.Services
 	public class DnsResolver : IDnsResolver
 	{
 		#region construction
-		
+
 		private readonly ILogger<DnsResolver> _logger;
+		private readonly Dictionary<(IPAddress, int), ILookupClient> _clients;
 
 		public DnsResolver(ILogger<DnsResolver> logger)
 		{
 			_logger = logger;
+			_clients = new Dictionary<(IPAddress, int), ILookupClient>();
+		}
+
+		#endregion
+
+		#region setup
+
+		private ILookupClient GetClient(string ip, int port)
+		{
+			var parsedIp = IPAddress.Parse(ip);
+			var key = (parsedIp, port);
+
+			var clientExists = _clients.TryGetValue(key, out var client);
+			if (!clientExists)
+			{
+				client = new LookupClient(key.parsedIp, key.port);
+				_clients.Add(key, client);
+			}
+
+			return client;
 		}
 
 		#endregion
 
 		public async Task<DnsQueryResult> ResolveDnsQuery(DnsQueryData data)
 		{
-			_logger.LogInformation($"Resolving DNS request for {data.RecordType} {data.Domain} with {data.Ip}:{data.Port}");
-			var ip = IPAddress.Parse(data.Ip);
-			var client = new LookupClient(ip, data.Port);
+			_logger.LogInformation(
+				$"Resolving DNS request for {data.RecordType} {data.Domain} with {data.Ip}:{data.Port}");
+			var client = GetClient(data.Ip, data.Port);
 
 			var type = MapQueryType(data.RecordType);
 
@@ -50,12 +72,33 @@ namespace DnsWatcher.Infrastructure.Services
 
 		private DnsQueryResult ProcessResult(DnsResourceRecord result)
 		{
-			var res = new DnsQueryResult { TimeToLive = -1 };
-			if (result is ARecord a)
+			var res = new DnsQueryResult {TimeToLive = -1};
+
+			if (result != null)
 			{
-				res.Value = a.Address.ToString();
-				res.TimeToLive = a.TimeToLive;
+				res.TimeToLive = result.TimeToLive;
+				if (result is ARecord a)
+				{
+					res.Value = a.Address.ToString();
+				}
+				else if (result is AaaaRecord aaaa)
+				{
+					res.Value = aaaa.Address.ToString();
+				}
+				else if (result is CNameRecord cname)
+				{
+					res.Value = cname.CanonicalName.ToString();
+				}
+				else if (result is MxRecord mx)
+				{
+					res.Value = mx.Exchange.ToString();
+				}
+				else if (result is TxtRecord txt)
+				{
+					res.Value = txt.Text.ToString();
+				}
 			}
+
 			return res;
 		}
 	}
