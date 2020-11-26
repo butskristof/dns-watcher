5b4ab76326ec92c7b8cc4e96499c3b41c383db69
Kristof Buts <kristof.buts@marqiz.com>
2020-11-26 23:06:43 +0100

    dashboard

--- src/pwa/src/app/main/main.module.ts
+++ src/pwa/src/app/main/main.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import {MainRoutingModule} from './main-routing.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.component';
+import {TranslateModule} from '@ngx-translate/core';
 
 @NgModule({
   declarations: [DashboardComponent, DashboardPageComponent],
   imports: [
     CommonModule,
-    MainRoutingModule
+    MainRoutingModule,
+    TranslateModule
   ]
 })
 export class MainModule { }
