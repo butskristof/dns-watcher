c321e389b97821af49e34944797d06cc8fd196c3
Kristof Buts <kristof.buts@marqiz.com>
2020-11-28 17:37:38 +0100

    styling

--- src/pwa/src/app/shared/components/header/header.component.ts
+++ src/pwa/src/app/shared/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../../../auth/services/auth.service';
+import {NavigationService} from '../../services/navigation.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private readonly authService: AuthService,
+    private readonly navigationService: NavigationService
+  ) { }
 
   ngOnInit(): void {
   }
 
+  // region getters
+  get isAuthenticated(): boolean {
+    return this.authService.isAuthenticated();
+  }
+  // endregion
+
+  // region actions
+  logout(): void {
+    this.authService.logout();
+    this.navigationService.goToLogin();
+  }
+  // endregion
 }
