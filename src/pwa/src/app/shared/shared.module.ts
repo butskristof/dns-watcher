c563ab41543dff4109dea955d906fa04ffffca9e
Kristof Buts <kristof.buts@marqiz.com>
2020-11-29 16:01:23 +0100

    environment indicator

--- src/pwa/src/app/shared/shared.module.ts
+++ src/pwa/src/app/shared/shared.module.ts
@@ -4,15 +4,18 @@ import { HeaderComponent } from './components/header/header.component';
 import {RouterModule} from '@angular/router';
 import {TranslateModule} from '@ngx-translate/core';
 import { ActionButtonComponent } from './components/utilities/action-button/action-button.component';
+import { EnvironmentIndicatorComponent } from './components/utilities/environment-indicator/environment-indicator.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
     ActionButtonComponent,
+    EnvironmentIndicatorComponent,
   ],
   exports: [
     HeaderComponent,
     ActionButtonComponent,
+    EnvironmentIndicatorComponent,
   ],
   imports: [
     CommonModule,
